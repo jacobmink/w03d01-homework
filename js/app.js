974012c2f701eb85b3d3a6f82c9555f43e392b4f
Jacob Mink <jacobmink@gmail.com>
2019-01-02 14:35:14 -0700

    Time interval working, stats increase with time

--- js/app.js
+++ js/app.js
@@ -13,29 +13,53 @@ class Pet {
 const myPet = new Pet(10,1,0,0,0);
 
 const game = {
-    time: 100
+    time: 0
 }
 
 const hungerIncrease = ()=>{
-    myPet.hunger += 1;
+    myPet.hunger++;
+    $('.hunger').text(`${myPet.hunger}`);
 }
 
 const morph = ()=>{
     $('#character').html("<img src='gremlin.jpg'>")
 }
 
+const boredomIncrease = ()=>{
+    myPet.boredom++;
+    $('.boredom').text(`${myPet.boredom}`)
+}
+const ageIncrease = ()=>{
+    myPet.age++;
+    $('.age').text(`${myPet.age}`)
+}
+const sleepinessIncrease = ()=>{
+    myPet.sleepiness++;
+    $('.sleepiness').text(`${myPet.sleepiness}`)
+}
+
 const timePasses = ()=>{
-    if (game.time === 100){
+    game.time++;
+    console.log(game.time)
+
+    if (myPet.age === 5){
         morph();
     }
     if (game.time % 3 === 0){
-        hungerIncrease(myPet);
+        hungerIncrease();
     }
-    
-    boredomIncrease();
-    ageIncrease();
-    sleepinessIncrease();
+    if (game.time % 5 === 0){
+        boredomIncrease();
+    }
+    if (game.time % 20 === 0){
+        ageIncrease();
+    }
+    if (game.time % 10 === 0){
+        sleepinessIncrease();
+    }
 }
+
+const timePassing = setInterval(timePasses,1000);
 
 const feed = ()=>{
     myPet.hunger -= 1;
@@ -54,22 +78,13 @@ const play = ()=>{
 
 const createBoard = ()=>{
     const $buttonBar = $('<div/>').attr('id','buttons');
-    const $statsBar = $('<div/>').attr('id','stats').html(`Age: ${myPet.age}\nHunger: ${myPet.hunger}\nBoredom: ${myPet.boredom}\nSleepiness: ${myPet.sleepiness}`);
+    const $statsBar = $('<div/>').attr('id','stats').html(`Age: <span class='age'>${myPet.age}</span>\nHunger: <span class='hunger'>${myPet.hunger}</span>\nBoredom: <span class='boredom'>${myPet.boredom}</span>\nSleepiness: <span class='sleepiness'>${myPet.sleepiness}</span>`);
     const $feedButton = $('<button/>').addClass('feeder').text('Feed Me').click(feed);
     const $sleepButton = $('<button/>').addClass('sleeper').text('Put me to sleep').click(sleep);
     const $playButton = $('<button/>').addClass('player').text('Play with me').click(play);
     $buttonBar.append($feedButton,$playButton,$sleepButton);
     $('body').append($buttonBar,$statsBar);
+    timePasses();
 }
 
-const startGame = ()=>{
-    const $startButton = $('<button/>').text('Start Game!').click(
-        (e)=>{
-            $(e.target).hide();
-            createBoard();
-        }
-        );
-    $('body').append($startButton);
-}
-
-startGame();
+createBoard();
